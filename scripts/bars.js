40ca913c7408cb8f8067c36fe728ef6881a824b2
Solin Official <solinsolinov@gmail.com>
2022-10-21 03:03:04 +0200

    Rework

--- scripts/bars.js
+++ scripts/bars.js
@@ -1,7 +1,13 @@
-function change(crypto, cryptoQR) {
+function change(crypto) {
     const bar = document.getElementById(crypto)
-    const qr = document.getElementById(cryptoQR)
+    const attribute = document.getElementById(crypto).attributes
 
-    bar.classList.toggle('donate__bar--extended')
-    qr.classList.toggle('hidden')
+    bar.classList.toggle('hidden')
+
+    if (attribute['aria-hidden'].value == 'true') {
+        bar.setAttribute('aria-hidden', 'false');
+    }
+    else {
+        bar.setAttribute('aria-hidden', 'true');
+    }
 }
